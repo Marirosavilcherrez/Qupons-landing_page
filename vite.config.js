889b05ebb02dff44f89168f871df074e3fe44f3d
Marirosa <arqmvilcherrez@gmail.com>
2024-02-19 12:14:36 -0500

    modified base in config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/qupons-landing/'
+  base:'/Qupons-landing_page/'
   //base:'https://pablobautistagoyoneche.gitlab.io/qupons-landing'
 })
